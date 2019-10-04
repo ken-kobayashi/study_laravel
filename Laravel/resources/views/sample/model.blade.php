a44bf018b76f7fd2ea0387cd348cb13981fb749b
vagrant <vagrant@homestead>
2019-10-04 15:36:25 +0000

    add model

---
+++ Laravel/resources/views/sample/model.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>model sample</title>
+</head>
+
+<body>
+  <ul>
+    @foreach($data as $d)
+      <li>{{$d->name}}<li>
+    @endforeach
+  </ul>
+</body>
+</html>
